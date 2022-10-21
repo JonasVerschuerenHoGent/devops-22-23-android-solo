06d4cb9f0004de87d5792c955fe568a3686f6ef3
jonas verschueren <jonas.verschueren@student.hogent.be>
2022-10-21 15:30:31 +0200

    Started with 'List of users'

--- app/src/main/java/com/example/test/LoginFragment.kt
+++ app/src/main/java/com/example/test/LoginFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.test.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -24,5 +25,9 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_listUsersFragment)
+        }
     }
 }
