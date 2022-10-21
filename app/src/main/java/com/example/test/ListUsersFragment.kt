e599794daca710ed54ca7c46a35a3728d05866ba
jonas verschueren <jonas.verschueren@student.hogent.be>
2022-10-21 15:30:31 +0200

    Started with 'List of users'

---
+++ app/src/main/java/com/example/test/ListUsersFragment.kt
@@ -0,0 +1,35 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.test.databinding.FragmentFirstBinding
+import com.example.test.databinding.FragmentListUsersBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class ListUsersFragment : Fragment() {
+
+    private lateinit var binding: FragmentListUsersBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list_users, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+
+}
