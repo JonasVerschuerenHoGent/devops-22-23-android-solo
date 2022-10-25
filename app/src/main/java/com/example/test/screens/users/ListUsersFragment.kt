3f10b59e1f427a93f115f487c0b0ea3ba9c045ca
JonasVerschuerenHoGent <jonas.verschueren@student.hogent.be>
2022-10-25 15:19:10 +0200

    Fixed error + clickable list of users

--- app/src/main/java/com/example/test/screens/users/ListUsersFragment.kt
+++ app/src/main/java/com/example/test/screens/users/ListUsersFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.test.R
-import com.example.test.databinding.FragmentListusersBinding
+import com.example.test.databinding.ListUsersFragmentBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -16,7 +20,7 @@ import com.example.test.databinding.FragmentListusersBinding
 class ListUsersFragment : Fragment() {
 
     //binding
-    private lateinit var binding: FragmentListusersBinding
+    private lateinit var binding: ListUsersFragmentBinding
 
     //viewModel
     private lateinit var viewModel: ListUsersViewModel
@@ -25,7 +29,7 @@ class ListUsersFragment : Fragment() {
                                savedInstanceState: Bundle?): View? {
 
         // Inflate view and obtain an instance of the binding class
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_listusers, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.list_users_fragment, container, false)
 
         //Get the viewModel
         viewModel = ViewModelProvider(this).get(ListUsersViewModel::class.java)
@@ -39,12 +43,23 @@ class ListUsersFragment : Fragment() {
         binding.setLifecycleOwner(this)
 
 
+        //Add items to the ListView and make it clickable
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, viewModel.listUsers.value!!)
+        binding.usersLv.adapter = arrayAdapter
+        binding.usersLv.onItemClickListener = object: AdapterView.OnItemClickListener{
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(requireContext(), "Clicked item = " + viewModel.listUsers.value!!.get(position), Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
     }
 
 
