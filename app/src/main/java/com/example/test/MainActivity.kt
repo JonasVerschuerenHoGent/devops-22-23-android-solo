d001c54da102c8050a9acc0ca876fbb74f02f20b
JonasVerschuerenHoGent <jonas.verschueren@student.hogent.be>
2022-12-05 17:34:07 +0100

    beginning of bottom-navbar

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -5,13 +5,18 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.test.databinding.ActivityMainBinding
 import com.example.test.screens.login.LoginActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private var prefs : SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO check when login is implement and only do this if an invalid token is provided
@@ -38,6 +44,29 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         NavigationUI.setupWithNavController(binding.navView, navController)
 
+        //setup bottom nav bar
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(navController)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.projects -> {
+                    Log.d("mainActivity", "projectlistfragment is clicked")
+                    NavigationUI.onNavDestinationSelected(it, navController)
+                }
+                R.id.virtualMachines -> {
+                    NavigationUI.onNavDestinationSelected(it, navController)
+                }
+                R.id.users -> {
+                    NavigationUI.onNavDestinationSelected(it, navController)
+                }
+                else -> false
+            }
+        }
+
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.drawerLayout, fragment).commit()
     }
 
     override fun onSupportNavigateUp(): Boolean {
