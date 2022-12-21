57742028347527bb852d0d6f1ec6104f6d582c9b
JonasVerschuerenHoGent <jonas.verschueren@student.hogent.be>
2022-12-21 01:18:50 +0100

    user detail screen finished

--- app/src/main/java/com/example/test/screens/users/UserFragment.kt
+++ app/src/main/java/com/example/test/screens/users/UserFragment.kt
@@ -6,12 +6,17 @@ import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.test.AccountMock
 import com.example.test.R
 import com.example.test.databinding.UserDetailFragmentBinding
 import com.example.test.domain.Account
+import com.example.test.screens.virtual_machines.*
 
 class UserFragment : Fragment() {
 
@@ -41,14 +46,35 @@ class UserFragment : Fragment() {
         // data in the VieWModel
         binding.usersViewModel = viewModel
 
+        //Add items to the ListView and make it clickable
+        arraylist = AccountMock().users
+        //binding.userNameLbl.text = arraylist[args.userId].name
+
+        val vmViewModelFactory = VirtualMachineListViewModelFactory();
+        val vmViewModel = ViewModelProvider(this, vmViewModelFactory).get(VirtualMachineListViewModel::class.java)
+        binding.virtualMachineListViewModel = vmViewModel
+
         // Specify the current activity as the lifecycle owner of the binding. This is used so that
         // the binding can observe LiveData updates
         binding.lifecycleOwner = this
 
+        val adapter = VirtualMachinesAdapter( VirtualMachineListener {
+                virtualMachineID ->
+            findNavController().navigate(VirtualMachineListFragmentDirections.
+            actionVirtualMachineListFragmentToVirtualMachineDetailFragment(
+                virtualMachineID
+            ))
+        })
 
-        //Add items to the ListView and make it clickable
-        arraylist = AccountMock().users
-        //binding.userNameLbl.text = arraylist[args.userId].name
+        val recyclerView = binding.virtualMachineList
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL)
+        )
+
+        vmViewModel.listVms.observe(viewLifecycleOwner, Observer {
+            println("List got ${it.size}")
+            adapter.submitList(it) })
 
 
         return binding.root
